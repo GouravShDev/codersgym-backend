d7ddc3ed59b96de03eb6f9e8bdc63088effb2635
Shubham Rawat <shubhamrwt2001@gmail.com>
2025-03-15 23:02:05 +0530

    project initial setup

---
+++ src/main/java/com/codersgym/codersgym_backend/CodersgymBackendApplication.java
@@ -0,0 +1,13 @@
+package com.codersgym.codersgym_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodersgymBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodersgymBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codersgym/codersgym_backend/CodersgymBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codersgym.codersgym_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodersgymBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
